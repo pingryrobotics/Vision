abd08a6d0ab9d557129ad8b797d5c1adc4e415b4
melwyn2021 <melwyn2021@pingry.org>
2019-02-27 21:16:52 -0500

    removed networktables core stuff

--- ntcore/gmock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Desktop/vision/ntcore/gmock
-# Build directory: /home/ubuntu/Desktop/vision/ntcore/gmock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
