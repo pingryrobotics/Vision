5243201612c0413b1a341898ca7ed9f42b6bc6ed
mcgrizzz <mcgrizzz@gmail.com>
2019-03-09 01:54:24 -0500

    for saturday

--- cScoreVision.py
+++ cScoreVision.py
@@ -1,6 +1,8 @@
 import networktables
+from ftplib import FTP
 from networktables import NetworkTables, NetworkTablesInstance
 import pathfinder as pf
+import cscore
 from cscore import CameraServer, VideoSource
 import cv2
 import numpy as np
@@ -9,64 +11,6 @@ import time as t
 import os
 import threading
 from grip import GripPipeline
-class WebcamVideoStream:
-    def __init__(self, camera, cameraServer, frameWidth, frameHeight, name="WebcamVideoStream"):
-        # initialize the video camera stream and read the first frame
-        # from the stream
-
-        #Automatically sets exposure to 0 to track tape
-        self.webcam = camera
-        self.webcam.setExposureManual(0)
-        #Some booleans so that we don't keep setting exposure over and over to the same value
-        self.autoExpose = False
-        self.prevValue = self.autoExpose
-        #Make a blank image to write on
-        self.img = np.zeros(shape=(frameWidth, frameHeight, 3), dtype=np.uint8)
-        #Gets the video
-        self.stream = cameraServer.getVideo()
-        (self.timestamp, self.img) = self.stream.grabFrame(self.img)
-
-        # initialize the thread name
-        self.name = name
-
-        # initialize the variable used to indicate if the thread should
-        # be stopped
-        self.stopped = False
-
-    def start(self):
-        # start the thread to read frames from the video stream
-        t = Thread(target=self.update, name=self.name, args=())
-        t.daemon = True
-        t.start()
-        return self
-
-    def update(self):
-        # keep looping infinitely until the thread is stopped
-        while True:
-            # if the thread indicator variable is set, stop the thread
-            if self.stopped:
-                return
-            #Boolean logic we don't keep setting exposure over and over to the same value
-            if self.autoExpose:
-                if(self.autoExpose != self.prevValue):
-                    self.prevValue = self.autoExpose
-                    self.webcam.setExposureAuto()
-            else:
-                if (self.autoExpose != self.prevValue):
-                    self.prevValue = self.autoExpose
-                    self.webcam.setExposureManual(0)
-            #gets the image and timestamp from cameraserver
-            (self.timestamp, self.img) = self.stream.grabFrame(self.img)
-
-    def read(self):
-        # return the frame most recently read
-        return self.timestamp, self.img
-
-    def stop(self):
-        # indicate that the thread should be stopped
-        self.stopped = True
-    def getError(self):
-        return self.stream.getError()
 def calculateDistance(heightOfCamera, heightOfTarget, pitch):
     heightOfTargetFromCamera = heightOfTarget - heightOfCamera
 
@@ -104,76 +48,80 @@ def calculatePitch(pixelY, centerY, vFocalLength):
     # Just stopped working have to do this:
     pitch *= -1
     return pitch
-def main():
-	# start NetworkTables
-	ntinst = NetworkTablesInstance.getDefault()
-	#Name of network table - this is how it communicates with robot. IMPORTANT
-	networkTable = NetworkTables.getTable('ChickenVision')
+#image size ratioed to 16:9
+image_width = 320
+image_height = 240
+# start NetworkTables
+ntinst = NetworkTablesInstance.getDefault()
+#Name of network table - this is how it communicates with robot. IMPORTANT
+NetworkTables.initialize(server='10.25.77.1')
+networkTable = NetworkTables.getTable('Vision')
 
-	#Setting up CS stuff
-	cs = CameraServer.getInstance()
-	cs.enableLogging()
+#Setting up CS stuff
+cs = CameraServer.getInstance()
+cs.enableLogging()
+print(cscore.getUsbCameraPath(1))
+print("^^path")
+#todo: find camera by path, lifecam doesn't have IDs so do by path
+camera = cs.startAutomaticCapture(name="cam0", path='/dev/v4l/by-path/platform-tegra-xhci-usb-0:3.3:1.0-video-index0')
+#camera = cs.startAutomaticCapture(dev=0)
 
-	#todo: find camera by path, lifecam doesn't have IDs so do by path
-	camera = cs.startAutomaticCapture(name="cam0", path='/dev/v4l/by-path/platform-tegra-xhci-usb-0:3.3:1.0-video-index0')
-	camera.setResolution(320, 240)
+camera.setResolution(320, 240)
+print("set resolution successfully")
+cap = cs.getVideo()
+print("got cvSink")
 
-	cap = WebcamVideoStream(camera,cs,image_width,image_height).start()
+#Send video back to Dashboard
+outputStream = cs.putVideo("Name", 320, 240)
 
-	#Send video back to Dashboard
-	outputStream = cs.putVideo("Name", 320, 240)
+#More efficient?
+img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
+#Start grip pipeline for processing
+pipeline = GripPipeline()
 
-	#More efficient?
-	img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
+#Lifecam 3000 from datasheet
+#Datasheet: https://dl2jx7zfbtwvr.cloudfront.net/specsheets/WEBC1010.pdf
+diagonalView = math.radians(68.5)
 
-	#Start grip pipeline for processing
-	pipeline = GripPipeline()
+#16:9 aspect ratio
+horizontalAspect = 16
+verticalAspect = 9
 
+#Reasons for using diagonal aspect is to calculate horizontal field of view.
+diagonalAspect = math.hypot(horizontalAspect, verticalAspect)
+#Calculations: http://vrguy.blogspot.com/2013/04/converting-diagonal-field-of-view-and.html
+horizontalView = math.atan(math.tan(diagonalView/2) * (horizontalAspect / diagonalAspect)) * 2
+verticalView = math.atan(math.tan(diagonalView/2) * (verticalAspect / diagonalAspect)) * 2
 
-	#image size ratioed to 16:9
-	image_width = 320
-	image_height = 240
+#Focal Length calculations: https://docs.google.com/presentation/d/1ediRsI-oR3-kwawFJZ34_ZTlQS2SDBLjZasjzZ-eXbQ/pub?start=false&loop=false&slide=id.g12c083cffa_0_165
+H_FOCAL_LENGTH = image_width / (2*math.tan((horizontalView/2)))
+V_FOCAL_LENGTH = image_height / (2*math.tan((verticalView/2)))
+CAMERA_HEIGHT = 18#inches
+VISION_TARGET_HEIGHT = 24.573#lower one
 
-	#Lifecam 3000 from datasheet
-	#Datasheet: https://dl2jx7zfbtwvr.cloudfront.net/specsheets/WEBC1010.pdf
-	diagonalView = math.radians(68.5)
-
-	#16:9 aspect ratio
-	horizontalAspect = 16
-	verticalAspect = 9
-
-	#Reasons for using diagonal aspect is to calculate horizontal field of view.
-	diagonalAspect = math.hypot(horizontalAspect, verticalAspect)
-	#Calculations: http://vrguy.blogspot.com/2013/04/converting-diagonal-field-of-view-and.html
-	horizontalView = math.atan(math.tan(diagonalView/2) * (horizontalAspect / diagonalAspect)) * 2
-	verticalView = math.atan(math.tan(diagonalView/2) * (verticalAspect / diagonalAspect)) * 2
-
-	#Focal Length calculations: https://docs.google.com/presentation/d/1ediRsI-oR3-kwawFJZ34_ZTlQS2SDBLjZasjzZ-eXbQ/pub?start=false&loop=false&slide=id.g12c083cffa_0_165
-	H_FOCAL_LENGTH = image_width / (2*math.tan((horizontalView/2)))
-	V_FOCAL_LENGTH = image_height / (2*math.tan((verticalView/2)))
-	CAMERA_HEIGHT = 18#inches
-	VISION_TARGET_HEIGHT = 24.573#lower one
-
-	start = t.time()
-	num_frames = 0
-    while True:
+start = t.time()
+num_frames = 0
+#cs.waitForever()
+print("got to start of loop")
+while True:
     	# Tell the CvSink to grab a frame from the camera and put it
-        # in the source image.  If there is an error notify the output.
-        time,img = cap.read()
-        screenHeight, screenWidth, _ = img.shape
+	# in the source image.  If there is an error notify the output.
+	time,img = cap.grabFrame(img)
+	print("grabbed frame")
+	screenHeight, screenWidth, _ = img.shape
 	centerX = (screenWidth / 2) - .5
 	centerY = (screenHeight / 2) - .5
-        if time == 0:
+	if time == 0:
 		# Send the output the error.
 		outputStream.notifyError(cap.stream.getError());
 		# skip the rest of the current iteration
 		continue
-       	pipeline.process(img)
+	pipeline.process(img)
 	contours = pipeline.filter_contours_output
 	cntsSorted = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
-        if len(cntsSorted) >= 2:
-		largestTwoContours = cntsSorted[2:]
-		center = [0,0,0,0]
+	if len(cntsSorted) >= 2:
+		largestTwoContours = cntsSorted[:2]
+		center = [0,0,0,0,0,0]
 		x1,y1,w1,h1 = cv2.boundingRect(largestTwoContours[0])
 		x2,y2,w2,h2 = cv2.boundingRect(largestTwoContours[1])
 		i = 0
@@ -189,8 +137,8 @@ def main():
 			cv2.circle(img, (cX, cY), 7, (255, 0, 0), -1)
 			x,y,w,h= cv2.boundingRect(c)
 			cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-			center[i] += cX
-			center[i+1] += cY
+			center[i] = cX
+			center[i+1] = cY
 			i=i+2
 		center[4] = int((center[0]+center[2])/2)
 		center[5] = int((center[1]+center[3])/2)
@@ -204,27 +152,36 @@ def main():
 		print("Distance2: " + str(calculatedDistance2) + " inches")
 		calculatedAngleCenter = calculateYaw(center[4],centerX,H_FOCAL_LENGTH)
 		calculatedDistanceCenter = calculateDistance(CAMERA_HEIGHT,VISION_TARGET_HEIGHT,calculatePitch(center[5],centerY,V_FOCAL_LENGTH))
-		worldX1 = calculatedDistance1*math.sin(calculatedAngle1)
-		worldY1 = calculatedDistance1*math.cos(calculatedAngle1)
-		worldX2 = calculatedDistance2*math.sin(calculatedAngle2)
-		worldY2 = calculatedDistance2*math.cos(calculatedAngle2)
-		theta = math.atan2(worldY2-worldY1,worldX2-worldX1)
-		worldCenterX = calculatedDistanceCenter*math.sin(calculatedAngleCenter)
-		worldCenterY = calculatedDistanceCenter*math.cos(calculatedAngleCenter)
-		print(worldCenterX)
-		print(worldCenterY)
-		print(theta)
-		points = [
-			pf.Waypoint(0, 0, math.radians(0)),
-			pf.Waypoint(worldCenterX, worldCenterY, theta)
-		]
-		info, trajectory = pf.generate(points, pf.FIT_HERMITE_CUBIC, pf.SAMPLES_HIGH,
-		dt=0.05, # 50ms
-		max_velocity=24.0,
-		max_acceleration=10.0,
-		max_jerk=60.0
-			)
-        outputStream.putFrame(img)
-        num_frames +=1
-	end = t.time()
-	#print("fps: " + str(num_frames/(end - start)))
+		print(calculatedDistanceCenter)
+		print(calculatedAngleCenter)
+		try:
+			worldX1 = calculatedDistance1*math.sin(calculatedAngle1)
+			worldY1 = calculatedDistance1*math.cos(calculatedAngle1)
+			worldX2 = calculatedDistance2*math.sin(calculatedAngle2)
+			worldY2 = calculatedDistance2*math.cos(calculatedAngle2)
+			theta = math.atan2(worldY2-worldY1,worldX2-worldX1)
+			worldCenterX = calculatedDistanceCenter * math.sin(calculatedAngleCenter)
+			worldCenterY = calculatedDistanceCenter * math.cos(calculatedAngleCenter)
+			print(worldCenterX)
+			print(worldCenterY)
+			print(theta)
+		except:
+			print("random nonetype")
+		try:
+			points = [pf.Waypoint(0, 0, math.radians(0)),pf.Waypoint(worldCenterX, worldCenterY, theta)]
+			info, trajectory = pf.generate(points, pf.FIT_HERMITE_CUBIC, pf.SAMPLES_HIGH,
+			dt=0.05, # 50ms
+			max_velocity=24.0,
+			max_acceleration=10.0,
+			max_jerk=60.0
+				)
+			pf.serialize("newTrajectory.bin",trajectory)
+			with FTP("roboRIO-2577-frc.local") as ftp:
+				ftp.login()
+				ftp.storbinary('STOR newTrajectory.bin',open("newTrajectory.bin",mode='rb'))
+		except:
+			print("pathfinder exception")
+#outputStream.putFrame(img)
+num_frames +=1
+end = t.time()
+print("fps: " + str(num_frames/(end - start)))
---
+++ runAtStart.sh
@@ -0,0 +1,1 @@
+python3 -m cscore --team 2577 cScoreVision.py:main
