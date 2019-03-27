a095538056c9c2c35402a8f40916acfba684a9a1
mcgrizzz <mcgrizzz@gmail.com>
2019-03-27 16:03:38 -0400

    working for mongomery

--- runAtStart.sh
+++ runAtStart.sh
@@ -1,3 +1,3 @@
-v4l2-ctl --set-ctrl=exposure_auto=1
-v4l2-ctl --set-ctrl=exposure_absolute=10
-python3 /home/ubuntu/Desktop/Vision/runGrip.py
+v4l2-ctl -d /dev/video0 --set-ctrl=exposure_auto=1
+v4l2-ctl -d /dev/video0 --set-ctrl=exposure_absolute=10
+python3 /home/ubuntu/Desktop/Vision/lineFollowingTest.py
